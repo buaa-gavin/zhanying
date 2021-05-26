a7cbd6a0b318cd4a63259391bb45f442da26cfd5
buaa-gavin <3312321558@qq.com>
2021-05-26 15:26:28 +0800

    add result.vue

---
+++ frontend/src/views/Result.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <Nav></Nav>
+    <div></div>
+    <Footer></Footer>
+  </div>
+</template>
+
+<script>
+import Nav from "../components/Nav";
+import Footer from "../components/Footer";
+import axios from "axios";
+export default {
+  components: {
+    Nav,
+    Footer,
+  },
+};
+</script>
+
+<style></style>
